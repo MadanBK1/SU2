2ec4f62665ba2d67f1300e724d144fd1ae75bd82
Madan B K <85264878+MadanBK1@users.noreply.github.com>
2026-09-14 00:50:28 -0400

    Add portable Kokkos GPU-aware MPI halo exchange

--- Common/src/linear_algebra/CSysVectorKokkos.cpp
+++ Common/src/linear_algebra/CSysVectorKokkos.cpp
@@ -34,4 +34,53 @@ void CSysVector<ScalarType>::GPUSetVal(ScalarType val, bool trigger) const {
   if (trigger && nElm > 0) Kokkos::deep_copy(DeviceView<ScalarType>(d_vec_val, nElm), val);
 }
 
+template <class ScalarType>
+void CSysVector<ScalarType>::PrepareKokkosMPIBuffers(unsigned long sendPoints, unsigned long recvPoints) {
+  using memory_space = typename Kokkos::DefaultExecutionSpace::memory_space;
+  const auto sendValues = sendPoints * nVar;
+  const auto recvValues = recvPoints * nVar;
+
+  if (sendValues > kokkos_mpi_send_capacity) {
+    if (d_kokkos_mpi_send != nullptr) Kokkos::kokkos_free(d_kokkos_mpi_send);
+    d_kokkos_mpi_send = static_cast<ScalarType*>(
+        Kokkos::kokkos_malloc<memory_space>("SU2::KokkosMPISendBuffer", sendValues * sizeof(ScalarType)));
+    kokkos_mpi_send_capacity = sendValues;
+  }
+  if (recvValues > kokkos_mpi_recv_capacity) {
+    if (d_kokkos_mpi_recv != nullptr) Kokkos::kokkos_free(d_kokkos_mpi_recv);
+    d_kokkos_mpi_recv = static_cast<ScalarType*>(
+        Kokkos::kokkos_malloc<memory_space>("SU2::KokkosMPIRecvBuffer", recvValues * sizeof(ScalarType)));
+    kokkos_mpi_recv_capacity = recvValues;
+  }
+  if (sendPoints > kokkos_mpi_send_index_capacity) {
+    if (d_kokkos_mpi_send_indices != nullptr) Kokkos::kokkos_free(d_kokkos_mpi_send_indices);
+    d_kokkos_mpi_send_indices = static_cast<unsigned long*>(
+        Kokkos::kokkos_malloc<memory_space>("SU2::KokkosMPISendIndices", sendPoints * sizeof(unsigned long)));
+    kokkos_mpi_send_index_capacity = sendPoints;
+  }
+  if (recvPoints > kokkos_mpi_recv_index_capacity) {
+    if (d_kokkos_mpi_recv_indices != nullptr) Kokkos::kokkos_free(d_kokkos_mpi_recv_indices);
+    d_kokkos_mpi_recv_indices = static_cast<unsigned long*>(
+        Kokkos::kokkos_malloc<memory_space>("SU2::KokkosMPIRecvIndices", recvPoints * sizeof(unsigned long)));
+    kokkos_mpi_recv_index_capacity = recvPoints;
+  }
+}
+
+template <class ScalarType>
+void CSysVector<ScalarType>::ReleaseKokkosMPIBuffers() {
+  if (d_kokkos_mpi_send != nullptr) Kokkos::kokkos_free(d_kokkos_mpi_send);
+  if (d_kokkos_mpi_recv != nullptr) Kokkos::kokkos_free(d_kokkos_mpi_recv);
+  if (d_kokkos_mpi_send_indices != nullptr) Kokkos::kokkos_free(d_kokkos_mpi_send_indices);
+  if (d_kokkos_mpi_recv_indices != nullptr) Kokkos::kokkos_free(d_kokkos_mpi_recv_indices);
+
+  d_kokkos_mpi_send = nullptr;
+  d_kokkos_mpi_recv = nullptr;
+  d_kokkos_mpi_send_indices = nullptr;
+  d_kokkos_mpi_recv_indices = nullptr;
+  kokkos_mpi_send_capacity = 0;
+  kokkos_mpi_recv_capacity = 0;
+  kokkos_mpi_send_index_capacity = 0;
+  kokkos_mpi_recv_index_capacity = 0;
+}
+
 template class CSysVector<su2mixedfloat>;
